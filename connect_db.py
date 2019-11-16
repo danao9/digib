37294011f7bb9cf7b84908073e613558fb1cc20d
Dana Cosman <cosman.dana@gmail.com>
2019-11-16 13:16:22 +0100

    create functions

--- connect_db.py
+++ connect_db.py
@@ -1,19 +1,38 @@
 import pymysql
 
-if __name__ == "__main__":
+
+def main():
+    #fetch_from_db()
+    insert_into_db()
+
+
+def fetch_from_db():
+
+    # connect to database
     db = pymysql.connect("35.190.197.53", "root", "public16", "digib")
 
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
+    cursor.execute("SELECT * FROM test")
+
+    # Fetch a single row using fetchone() method.
+    data = cursor.fetchone()
+    print(data)
+
+    # disconnect from server now
+    db.close()
+
+def insert_into_db():
+
+    # connect to database
+    db = pymysql.connect("35.190.197.53", "root", "public16", "digib")
 
     # execute SQL query using execute() method.
-    sql0 = """SELECT * FROM test"""
     sql = """ INSERT INTO test (product, sales)
-    VALUES ("shampoo",45), ("ariel",678) """
+        VALUES ("solution",65), ("conditioner",78) """
 
-    # # Fetch a single row using fetchone() method.
-    # data = cursor.fetchone()
-    # print(data)
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
 
     try:
         # Execute the SQL command
@@ -28,3 +47,6 @@ if __name__ == "__main__":
     # disconnect from server now
     db.close()
 
+
+if __name__ == "__main__":
+    main()
